de81ec8e5a25e2147f7acef4a196e17a8c5ef77e
perqin <perqinxie@gmail.com>
2017-04-05 21:27:52 +0800

    Handle permission of drawing over other apps

--- app/src/main/java/com/perqin/gandear/FloatingWindowService.java
+++ app/src/main/java/com/perqin/gandear/FloatingWindowService.java
@@ -2,11 +2,19 @@ package com.perqin.gandear;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class FloatingWindowService extends Service {
     private static final String TAG = "FloatingWindowService";
+
+    private TextView textView;
+    private WindowManager windowManager;
 
     public FloatingWindowService() {
     }
@@ -14,7 +22,22 @@ public class FloatingWindowService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i(TAG, "onCreate");
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        textView = new TextView(this);
+        textView.setBackgroundColor(Color.RED);
+        textView.setText(R.string.app_name);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+        lp.gravity = Gravity.TOP | Gravity.LEFT;
+        lp.x = 100;
+        lp.y = 100;
+        windowManager.addView(textView, lp);
     }
 
     @Override
@@ -25,8 +48,11 @@ public class FloatingWindowService extends Service {
 
     @Override
     public void onDestroy() {
-        Log.i(TAG, "onDestroy");
         super.onDestroy();
+        if (textView != null) {
+            windowManager.removeView(textView);
+            textView = null;
+        }
     }
 
     @Override
--- app/src/main/java/com/perqin/gandear/SettingsActivity.java
+++ app/src/main/java/com/perqin/gandear/SettingsActivity.java
@@ -1,12 +1,16 @@
 package com.perqin.gandear;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
@@ -18,6 +22,8 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragment {
+        private static final int REQUEST_MANAGE_OVERLAY_PERMISSION = 6565;
+
         private SwitchPreference mServiceEnabledPreference;
 
         @Override
@@ -29,18 +35,63 @@ public class SettingsActivity extends AppCompatActivity {
             PreferenceScreen preferenceScreen = getPreferenceScreen();
             mServiceEnabledPreference = (SwitchPreference)
                     preferenceScreen.findPreference(getString(R.string.pk_service_enabled));
-            mServiceEnabledPreference.setChecked(
-                    ServiceUtils.isServiceRunning(getActivity(), FloatingWindowService.class));
             mServiceEnabledPreference.setOnPreferenceChangeListener((preference, newValue) -> {
                 Boolean checked = (Boolean) newValue;
-                Intent intent = new Intent(getActivity(), FloatingWindowService.class);
+                // Avoid restart or re-stop service
+                if (checked == ServiceUtils.isServiceRunning(getActivity(), FloatingWindowService.class)) {
+                    return true;
+                }
                 if (checked) {
-                    getActivity().startService(intent);
+                    // We have to handle permission here
+                    tryStartingFloatingWindowService();
                 } else {
+                    Intent intent = new Intent(getActivity(), FloatingWindowService.class);
                     getActivity().stopService(intent);
                 }
                 return true;
             });
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            mServiceEnabledPreference.setChecked(
+                    ServiceUtils.isServiceRunning(getActivity(), FloatingWindowService.class));
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (REQUEST_MANAGE_OVERLAY_PERMISSION == requestCode) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!Settings.canDrawOverlays(getActivity())) {
+                        // User refuse to grant permission :(
+                        Toast.makeText(getActivity(), R.string.you_must_allow_me_to_draw_over_other_apps, Toast.LENGTH_SHORT).show();
+                    } else {
+                        startFloatingWindowService();
+                    }
+                }
+            }
+        }
+
+        private void tryStartingFloatingWindowService() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // "Draw over other apps" has to be enable manually by user on Android M
+                // if the app is not installed from Google Play Store.
+                if (!Settings.canDrawOverlays(getActivity())) {
+                    Intent permissionIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + "com.perqin.gandear"));
+                    startActivityForResult(permissionIntent, REQUEST_MANAGE_OVERLAY_PERMISSION);
+                } else {
+                    startFloatingWindowService();
+                }
+            } else {
+                startFloatingWindowService();
+            }
+        }
+
+        private void startFloatingWindowService() {
+            Intent intent = new Intent(getActivity(), FloatingWindowService.class);
+            getActivity().startService(intent);
         }
     }
 }
